b0d7f620af02314da709f234a1cf14ad660fcbf5
Jaime Gomez <jaimegj99@gmail.com>
2019-10-30 17:33:39 +0100

    Update LDR_Test.py

--- scripts/LDR_Test.py
+++ scripts/LDR_Test.py
@@ -1,4 +1,28 @@
-from gpiozero import LightSensor
-ldr = LightSensor(4)
-while True:
-	print(ldr.value)
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+pin_to_circuit = 26
+
+def rc_time(pin_to_circuit):
+  count = 0
+
+  GPIO.setup(pin_to_circuit, GPIO.OUT)
+  GPIO.output(pin_to_circuit, GPIO.LOW)
+  time.sleep(0.1)
+
+  GPIO.setup(pin_to_circuit, GPIO.IN)
+
+  while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+    count += 1
+
+  return count
+
+try:
+  while True:
+    print rc_time(pin_to_circuit)
+except KeyboardInterrupt:
+  pass
+finally:
+  GPIO.cleanup()
